991f133c6a646bb07973c8d5f5749f4689f10d1e
Ali Albarrak <alaa13212@gmail.com>
2026-02-27 07:04:33 +0300

    Add migration for column sizes fix

---
+++ src/Sigil.Infrastructure/Migrations/20260227040320_IncreaseColumnSizes.cs
@@ -0,0 +1,216 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sigil.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    internal partial class IncreaseColumnSizes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Module",
+                table: "StackFrames",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(300)",
+                oldMaxLength: 300,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Function",
+                table: "StackFrames",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(300)",
+                oldMaxLength: 300,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Filename",
+                table: "StackFrames",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(300)",
+                oldMaxLength: 300,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Issues",
+                type: "character varying(8192)",
+                maxLength: 8192,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SuggestedFramesSummary",
+                table: "Issues",
+                type: "character varying(8192)",
+                maxLength: 8192,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(2000)",
+                oldMaxLength: 2000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SuggestedEventMessage",
+                table: "Issues",
+                type: "character varying(8192)",
+                maxLength: 8192,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Culprit",
+                table: "Issues",
+                type: "character varying(8192)",
+                maxLength: 8192,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "Events",
+                type: "character varying(8192)",
+                maxLength: 8192,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Culprit",
+                table: "Events",
+                type: "character varying(8192)",
+                maxLength: 8192,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Module",
+                table: "StackFrames",
+                type: "character varying(300)",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Function",
+                table: "StackFrames",
+                type: "character varying(300)",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Filename",
+                table: "StackFrames",
+                type: "character varying(300)",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Issues",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(8192)",
+                oldMaxLength: 8192,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SuggestedFramesSummary",
+                table: "Issues",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(8192)",
+                oldMaxLength: 8192,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SuggestedEventMessage",
+                table: "Issues",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(8192)",
+                oldMaxLength: 8192,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Culprit",
+                table: "Issues",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(8192)",
+                oldMaxLength: 8192,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "Events",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(8192)",
+                oldMaxLength: 8192,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Culprit",
+                table: "Events",
+                type: "character varying(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(8192)",
+                oldMaxLength: 8192,
+                oldNullable: true);
+        }
+    }
+}
